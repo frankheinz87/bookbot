d090fd6bf96d2f15074fbe2c66529eda02f8f90e
frankheinz87 <frankheinz1987@gmail.com>
2025-05-17 15:59:37 +0200

    implemented get_num_chars in stats.py

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 def main (filepath):
     book = get_book_text (filepath)
     num_words = get_num_words (book)
+    num_chars = get_num_chars (book)
     print (f"{num_words} words found in the document")
+    print (num_chars)
      
 def get_book_text (filepath):
     with open(filepath) as file_object:
@@ -9,5 +11,7 @@ def get_book_text (filepath):
     return book_text
 
 from stats import get_num_words
+
+from stats import get_num_chars
 
 main("books/frankenstein.txt")
--- stats.py
+++ stats.py
@@ -2,3 +2,18 @@ def get_num_words (text):
     words=text.split()
     cnt_words= len(words)
     return cnt_words
+
+def get_num_chars (text):
+    nocaps_text=text.lower()
+    chars=list(nocaps_text) 
+    result={}
+    cnt_char=int
+    for char in chars:
+        if char not in result:
+            cnt_char=1
+            result[f"{char}"]=cnt_char
+        else :
+            cnt_char=result[f"{char}"]
+            cnt_char+=1
+            result[f"{char}"]=cnt_char
+    return result
